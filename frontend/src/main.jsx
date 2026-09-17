91bf760741311f59032fac5c0c4ce5d148aef031
Harshraj singh gour <harshrajsinghgour1@gmail.com>
2026-09-17 08:19:28 +0530

    fix: use validated production frontend entry

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./AppProduction";
 import "./index.css";
 
 if ("serviceWorker" in navigator && import.meta.env.PROD) {
